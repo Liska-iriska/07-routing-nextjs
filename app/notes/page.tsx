3b6c08c2ba8cc3cd678e7f024817a2d1df334780
Lis <genia.grishenko@gmail.com>
2026-06-04 14:56:01 +0300

    Fix

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -80,7 +80,7 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import { fetchNotes } from "../../lib/api";
-import NotesClient from "./Notes.client";
+import NotesClient from "./filter/[...slug]/Notes.client";
 
 export default async function Notes() {
   const queryClient = new QueryClient();
